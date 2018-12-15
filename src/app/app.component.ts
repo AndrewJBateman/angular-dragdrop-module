fbf57ac1886ceb44535f8f05dc66b89e76a4089a
Andrew Bateman <gomezbateman@yahoo.com>
2018-12-15 20:01:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dragdrop-module';
+  numbers: number[] = [];
+
+  constructor() {
+    for(let i=0; i<10000; i++) {
+      this.numbers.push(i);
+
+    }
+  }
 }
