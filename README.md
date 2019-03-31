d4df3f78cf311a11fe76996a2c623d41cacab3bd
Andrew Bateman <gomezbateman@yahoo.com>
2019-03-31 21:39:35 +0200

    drag and drop array of strings added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Angular Material Table
 
-This project uses the Angular 7 Component Dev Kit (cdk) Drag and Drop module to drag numbered boxes from a list on the left to a numbered list on the right and vice versa. [Angular documentation on observables](https://angular.io/guide/observables).
+This project uses the [@angular/cdk/drag-drop module](https://material.angular.io/cdk/drag-drop/overview)  to drag numbered boxes from a list on the left to a numbered list on the right and vice versa.
 
 *** Note: to open web links in a new window use: _ctrl+click on link_**
 
@@ -17,10 +17,7 @@ This project uses the Angular 7 Component Dev Kit (cdk) Drag and Drop module to 
 
 ## General info
 
-"The @angular/cdk/drag-drop module provides you with a way to easily and declaratively create drag-and-drop interfaces, with support for free dragging, sorting within a list, transferring items between lists, animations, touch devices, custom drag handles, previews, and placeholders, in addition to horizontal lists and locking along an axis."
-[Link to Angular documentation on Drag and Drop](https://material.angular.io/cdk/drag-drop/overview).
-
-Link to interesting Medium article by [Nwose Lotanna](https://blog.angularindepth.com/@viclotana): [Building Interactive Lists with the new Angular 7 Drag and Drop tool](https://blog.angularindepth.com/building-interactive-lists-with-the-new-angular-7-drag-and-drop-tool-5f2402f8cb27).
+"The [@angular/cdk/drag-drop module](https://material.angular.io/cdk/drag-drop/overview) provides you with a way to easily and declaratively create drag-and-drop interfaces, with support for free dragging, sorting within a list, transferring items between lists, animations, touch devices, custom drag handles, previews, and placeholders, in addition to horizontal lists and locking along an axis."
 
 ## Screenshots
 
@@ -30,6 +27,7 @@ Link to interesting Medium article by [Nwose Lotanna](https://blog.angularindept
 
 * [Angular CLI v7.0.6](https://github.com/angular/angular-cli)
 * [Angular Material v7.1.1](https://material.angular.io/)
+* [Angular Material Drag and Drop Module](https://material.angular.io/cdk/drag-drop/overview)
 
 ## Setup
 
@@ -49,13 +47,17 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 ## Status & To-Do List
 
-* Status: Project is working and ready to be expanded upon.
+* Status: Project is not working - dependencies need to be re-done.
 
 * To-Do:
 
 ## Inspiration
 
-Project inspired by Youtube tutorial: [Drag and Drop with the Angular CDK - What's New in v7](https://www.youtube.com/watch?v=t1CrWLGxQPk).
+* Project inspired by Youtube tutorial: [Drag and Drop with the Angular CDK - What's New in v7](https://www.youtube.com/watch?v=t1CrWLGxQPk).
+
+*[Tim Deschryver's Exploring Drag and Drop with the new Angular Material CDK](https://blog.angularindepth.com/exploring-drag-and-drop-with-the-angular-material-cdk-2e0237857290)
+
+* [Nwose Lotann´s Building Interactive Lists with the new Angular 7 Drag and Drop tool](https://blog.angularindepth.com/building-interactive-lists-with-the-new-angular-7-drag-and-drop-tool-5f2402f8cb27).
 
 ## Contact
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,112 @@
-import { Component } from '@angular/core';
-import { moveItemInArray, CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDragEnter,
+  CdkDragExit,
+  CdkDrag
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-dragdrop-module';
+  title = 'angular drag & drop';
   numbers: number[] = [];
   otherNumbers: number[] = [];
 
+  newItems = [
+  'Item 0',
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  ];
+
+  activeItems = [
+  'Item 4',
+  ];
+
+  doneItems = [
+  'Item 5',
+  'Item 6',
+  'Item 7',
+  ];
+
   // create list of numbers
-  constructor() {
-    for (let i = 0; i < 10; i++) {
-      this.numbers.push(i);
+  // constructor() {
+  //   for (let i = 0; i < 10; i++) {
+  //     this.numbers.push(i);
+  //   }
+  // }
+  // custom drop event, using the transferArrayItem and moveItemInArray methods
+  // previousContainer = Container from which the item got dragged.
+  // transferArrayItem is an imported utility function
+  drop(event: CdkDragDrop<number[]>) {
+    console.log(event.previousContainer.data); // initially returns array (10) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    console.log(event.container.data); // initially returns []
+
+      if (event.previousContainer !== event.container) { // check to see if moved across lists
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else { // move within the same array and change index
+      moveItemInArray(
+        this.numbers,
+        event.previousIndex,
+        event.currentIndex
+      );
     }
   }
-  drop(event: CdkDragDrop<number[]>) {
-    if (event.previousContainer !== event.container) {
-      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      moveItemInArray(this.numbers, event.previousIndex, event.currentIndex);
+
+  dropped(event: CdkDragDrop<string[]>) {
+    if (event.item.data === 'Try to move me') {
+      console.log('this isn\'t happening today');
+      return;
     }
 
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+      );
+    }
+  }
+
+  entered(event: CdkDragEnter<string[]>) {
+    console.log('Entered', event.item.data);
+  }
+
+  exited(event: CdkDragExit<string[]>) {
+    console.log('Exited', event.item.data);
+  }
+
+  specialUseCase(drag?: CdkDrag, drop?: CdkDrop) {
+    if (drop.data.length <= 2) {
+      console.log('Can\'t drop you because there aren\'t enough items in \'Active\'');
+      return false;
+    }
+
+    const allowedItems = ['Item 5', 'Item 6', 'Item 7', 'Item 2'];
+    if (allowedItems.indexOf(drag.data) === -1) {
+      console.log('Can\'t drop you because only Item 2, 5, 6 and 7 are allowed here');
+      return false;
+    }
+
+    return true;
   }
 }
